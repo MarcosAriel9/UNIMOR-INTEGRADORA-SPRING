dc41d5233e5785ce0609bec59f42772e07750246
MarcosAriel9 <20203tn043@utez.edu.mx>
2022-03-05 20:05:37 -0600

    first commit

---
+++ src/main/java/mx/edu/utez/unimor/unimor/UnimorApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.unimor.unimor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimorApplication.class, args);
+	}
+
+}
